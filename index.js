d9e2633d1af57b8388c51f3139b7ef22fa41805f
Karthik798m <karthikmanoj798m@gmail.com>
2024-08-23 00:08:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+
+document.querySelector(".btn1").addEventListener('click',function()
+{
+    
+    document.querySelector('.div1').classList.toggle('active2')
+    document.querySelector('.box1').classList.toggle('pop')
+    document.querySelector('.box2').classList.toggle('pop')
+})
+
+
+
+document.querySelector(".btn1").addEventListener('click', swap)
+
+function swap() {
+  var x = document.querySelector(".btn1");
+  if (x.innerHTML === "Sign Up") {
+    x.innerHTML = "Sign In";
+  } else {
+    x.innerHTML = "Sign Up";
+  }
+}
